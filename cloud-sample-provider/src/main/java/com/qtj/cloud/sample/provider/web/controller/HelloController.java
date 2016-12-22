eeb285b8d520c40f5a6898564eb69c8b70d09283
qtj <tianjieqin@126.com>
2016-12-22 19:32:02 +0800

    update

--- cloud-sample-provider/src/main/java/com/qtj/cloud/sample/provider/web/controller/HelloController.java
+++ cloud-sample-provider/src/main/java/com/qtj/cloud/sample/provider/web/controller/HelloController.java
@@ -3,6 +3,7 @@ package com.qtj.cloud.sample.provider.web.controller;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,11 +13,15 @@ import com.qtj.cloud.sample.service.HelloService;
 
 
 @RestController
+@RefreshScope
 @RequestMapping("/hello")
 public class HelloController {
 	
 	@Value("${server.port}")
 	private String port;
+	
+	@Value("${message:[Not from config center value.]}")
+    private String message;
 	
 	@Autowired
 	HelloService helloService;
@@ -24,9 +29,9 @@ public class HelloController {
 	@GetMapping("/name")
 	public String hello(@RequestParam(value="name", required=false) String name) {
 		if (StringUtils.isBlank(name)) {
-			return helloService.hello(port);
+			return helloService.hello(port + ", msg: " + message);
 		} else {
-			return helloService.hello(name + " - " + port);
+			return helloService.hello(name + " - " + port + ", msg: " + message);
 		}
 	}
 
