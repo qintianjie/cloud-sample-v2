dfa69033303bb06e88918c51d32898d1caca8e85
qtj <tianjieqin@126.com>
2016-12-22 17:21:45 +0800

    config server update

--- cloud-sample-configcenter/src/main/java/com/qtj/cloud/sample/config/center/CloudSampleConfigcenterApplication.java
+++ cloud-sample-configcenter/src/main/java/com/qtj/cloud/sample/config/center/CloudSampleConfigcenterApplication.java
@@ -2,8 +2,16 @@ package com.qtj.cloud.sample.config.center;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * http://localhost:8672/cloud-sample/default 
+ * @author qtj
+ *
+ */
 
 @SpringBootApplication
+@EnableConfigServer
 public class CloudSampleConfigcenterApplication {
 
 	public static void main(String[] args) {
